ce79ebb0a7b03664973bf82b67b82d2cddbf4826
ratautas <algirdas.tamasauskas@gmail.com>
2024-01-07 14:06:20 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-template-keys
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/ratautas/vue-template-keys)
---
+++ vue2/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <template v-for="item in list">
+      <div></div>
+      <div></div>
+    </template>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  components: {
+    HelloWorld,
+  },
+  data() {
+    return {
+      list: ['one', 'two', 'three'],
+    }
+  },
+}
+</script>
